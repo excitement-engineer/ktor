ea133df2268dd1b02de67e4bb25ebc040507dd25
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-09-24 19:33:13 +0300

    ~ try to redispatch selection to the request dispatcher

--- ktor-network/src/io/ktor/network/selector/ActorSelectorManager.kt
+++ ktor-network/src/io/ktor/network/selector/ActorSelectorManager.kt
@@ -92,16 +92,10 @@ class ActorSelectorManager(dispatcher: CoroutineContext) : SelectorManagerSuppor
         }
     }
 
-    private suspend fun dispatchIfNeeded() {
-        val dispatcher = coroutineContext.fold<CoroutineDispatcher?>(null) { acc, e ->
-            acc ?: (e as? CoroutineDispatcher)
-        }
-
-        if (dispatcher != null) {
-            if (dispatcher.isDispatchNeeded(coroutineContext)) {
-                yield()
-            }
-        }
+    private suspend inline fun dispatchIfNeeded() {
+        yield() // it will always redispatch it to the right thread
+        // it is very important here because we do _unintercepted_ resume that may lead to blocking on a wrong thread
+        // that may cause deadlock
     }
 
     private fun selectWakeup() {
@@ -173,7 +167,7 @@ class ActorSelectorManager(dispatcher: CoroutineContext) : SelectorManagerSuppor
         fun resume(value: R): Boolean {
             val continuation = ref.getAndSet(null)
             if (continuation != null) {
-                continuation.resume(value)
+                continuation.resume(value) /** we resume unintercepted, see [dispatchIfNeeded] */
                 return true
             }
 
